105c90e81692640712ba93d22c70a5165b7b9351
felipe-coletti <felipecoletti2@gmail.com>
2024-12-07 17:46:50 -0300

    chore(dark theme): update color for neutral primary text

--- packages/styles/src/themes/dark.ts
+++ packages/styles/src/themes/dark.ts
@@ -2,20 +2,19 @@ import { createTheme } from '../stitches.config'
 import colors from '@strawberry-ui/tokens/colors.json'
 
 export const darkTheme = createTheme({
-  colors: {
-    colorAccent: colors.brand.mid,
-    colorAccentHover: colors.brand.light,
-    colorNeutralBackdrop: `${colors.white}30`,
-    colorNeutralBorder: colors.gray[700],
-    colorNeutralDisabledBackground: colors.gray[950],
-    colorNeutralDisabledText: colors.gray[600],
-    colorNeutralHover: `${colors.gray[200]}10`,
-    colorNeutralPlaceholder: colors.gray[500],
-    colorNeutralPrimaryBackground: colors.gray[950],
-    colorNeutralSecondaryBackground: colors.gray[900],
-    colorNeutralSecondaryBackgroundHover: colors.gray[800],
-    colorNeutralPrimaryText: colors.gray[200],
-    colorNeutralSecondaryText: colors.gray[400]
-  }
+    colors: {
+        colorAccent: colors.brand.mid,
+        colorAccentHover: colors.brand.light,
+        colorNeutralBackdrop: `${colors.white}30`,
+        colorNeutralBorder: colors.gray[700],
+        colorNeutralDisabledBackground: colors.gray[950],
+        colorNeutralDisabledText: colors.gray[600],
+        colorNeutralHover: `${colors.gray[200]}10`,
+        colorNeutralPlaceholder: colors.gray[500],
+        colorNeutralPrimaryBackground: colors.gray[950],
+        colorNeutralSecondaryBackground: colors.gray[900],
+        colorNeutralSecondaryBackgroundHover: colors.gray[800],
+        colorNeutralPrimaryText: colors.white,
+        colorNeutralSecondaryText: colors.gray[400]
+    }
 })
-  
