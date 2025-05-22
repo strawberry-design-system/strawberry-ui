2e612494f41eaa45abedf4895159d84b5ad64ffe
felipe-coletti <felipecoletti2@gmail.com>
2025-05-22 13:38:48 -0300

    fix(modal): correct event type

--- packages/react/src/components/Modal/Modal.tsx
+++ packages/react/src/components/Modal/Modal.tsx
@@ -1,39 +1,35 @@
 import { useEffect } from 'react'
 import { ModalProps } from './Modal.types'
-import {
-    modalContainerStyle,
-    backdropStyle,
-    modalStyle,
-} from '@strawberry-ui/styles/components/Modal'
+import { modalContainerStyle, backdropStyle, modalStyle } from '@strawberry-ui/styles/components/Modal'
 
 export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
-    useEffect(() => {
-        if (isOpen) {
-            const handleEsc = (e: KeyboardEvent>) => {
-                if (e.key === 'Escape') {
-                    onClose()
-                }
-            }
+	useEffect(() => {
+		if (isOpen) {
+			const handleEsc = (e: KeyboardEvent) => {
+				if (e.key === 'Escape') {
+					onClose()
+				}
+			}
 
-            window.addEventListener('keydown', handleEsc)
+			window.addEventListener('keydown', handleEsc)
 
-            return () => window.removeEventListener('keydown', handleEsc)
-        }
-    }, [isOpen, onClose])
+			return () => window.removeEventListener('keydown', handleEsc)
+		}
+	}, [isOpen, onClose])
 
-    if (!isOpen) return null
+	if (!isOpen) return null
 
-    const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
-        if (e.target === e.currentTarget) {
-            onClose?.()
-        }
-    }
+	const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		if (e.target === e.currentTarget) {
+			onClose?.()
+		}
+	}
 
-    return (
-        <div className={modalContainerStyle()}>
-            <div className={backdropStyle()} onClick={handleOverlayClick}>
-                <div className={modalStyle()}>{children}</div>
-            </div>
-        </div>
-    )
+	return (
+		<div className={modalContainerStyle()}>
+			<div className={backdropStyle()} onClick={handleOverlayClick}>
+				<div className={modalStyle()}>{children}</div>
+			</div>
+		</div>
+	)
 }
