1ff86448ef5e5cecd2403f6087af1ed0d5c79ef9
felipe-coletti <felipecoletti2@gmail.com>
2025-06-06 21:07:48 -0300

    chore(color tokens): update gray.50 color value from #f5f5f5 to #f0f0f0

--- packages/styles/src/stitches.config.ts
+++ packages/styles/src/stitches.config.ts
@@ -8,40 +8,40 @@ import spacing from '@strawberry-ui/tokens/spacing.json'
 import typography from '@strawberry-ui/tokens/typography.json'
 
 export const { css, globalCss, keyframes, getCssText, theme, createTheme, config } = createStitches({
-    theme: {
-        colors: {
-            colorAccent: colors.brand.dark,
-            colorAccentHover: colors.brand.low,
-            colorNegative: colors.negative,
-            colorPositive: colors.positive,
-            colorWarning: colors.warning,
-            colorNeutralBackdrop: `${colors.black}30`,
-            colorNeutralBorder: colors.gray[300],
-            colorNeutralDisabledBackground: colors.gray[400],
-            colorNeutralDisabledText: colors.gray[800],
-            colorNeutralHover: `${colors.gray[800]}10`,
-            colorNeutralPlaceholder: colors.gray[500],
-            colorNeutralPrimaryBackground: colors.white,
-            colorNeutralSecondaryBackground: colors.gray[50],
-            colorNeutralSecondaryBackgroundHover: colors.gray[200],
-            colorNeutralPrimaryText: colors.gray[800],
-            colorNeutralSecondaryText: colors.gray[600]
-        },
-        space: spacing,
-        fontSizes: typography.fontSizes,
-        fonts: typography.fontFamily,
-        fontWeights: typography.fontWeight,
-        lineHeights: typography.lineHeight,
-        sizes: sizes,
-        borderWidths: border.widths,
-        borderStyles: border.style,
-        radii: border.radius,
-        shadows: shadows,
-        media: {
-            mobile: `(min-width: ${breakpoints.mobile})`,
-            tablet: `(min-width: ${breakpoints.tablet})`,
-            desktop: `(min-width: ${breakpoints.desktop})`,
-            largeDesktop: `(min-width: ${breakpoints.largeDesktop})`
-        }
-    }
+	theme: {
+		colors: {
+			colorAccent: colors.brand.dark,
+			colorAccentHover: colors.brand.low,
+			colorNegative: colors.negative,
+			colorPositive: colors.positive,
+			colorWarning: colors.warning,
+			colorNeutralBackdrop: `${colors.black}30`,
+			colorNeutralBorder: colors.gray[300],
+			colorNeutralDisabledBackground: colors.gray[400],
+			colorNeutralDisabledText: colors.gray[800],
+			colorNeutralHover: `${colors.gray[800]}10`,
+			colorNeutralPlaceholder: colors.gray[500],
+			colorNeutralPrimaryBackground: colors.white,
+			colorNeutralSecondaryBackground: colors.gray[50],
+			colorNeutralSecondaryBackgroundHover: colors.gray[200],
+			colorNeutralPrimaryText: colors.gray[800],
+			colorNeutralSecondaryText: colors.gray[600]
+		},
+		space: spacing,
+		fontSizes: typography.fontSizes,
+		fonts: typography.fontFamily,
+		fontWeights: typography.fontWeight,
+		lineHeights: typography.lineHeight,
+		sizes: sizes,
+		borderWidths: border.widths,
+		borderStyles: border.style,
+		radii: border.radius,
+		shadows: shadows,
+		media: {
+			mobile: `(min-width: ${breakpoints.mobile})`,
+			tablet: `(min-width: ${breakpoints.tablet})`,
+			desktop: `(min-width: ${breakpoints.desktop})`,
+			largeDesktop: `(min-width: ${breakpoints.largeDesktop})`
+		}
+	}
 })
