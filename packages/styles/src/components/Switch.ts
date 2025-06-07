75514174caec49f1b294a889ead04a22eb189a6a
felipe-coletti <felipecoletti2@gmail.com>
2025-06-06 21:10:32 -0300

    style(switch): change design from outline to filled

--- packages/styles/src/components/Switch.ts
+++ packages/styles/src/components/Switch.ts
@@ -1,84 +1,76 @@
 import { css } from '../stitches.config'
 
 export const labelStyle = css({
-    alignItems: 'center',
-    color: '$colorNeutralPrimaryText',
-    cursor: 'pointer',
-    display: 'flex',
-    fontSize: '$small',
-    gap: '$xsmall',
-    variants: {
-        disabled: {
-            true: {
-                color: 'colorNeutralDisabledText',
-                cursor: 'not-allowed',
-                opacity: 0.5,
-            },
-        },
-    },
+	alignItems: 'center',
+	color: '$colorNeutralPrimaryText',
+	cursor: 'pointer',
+	display: 'flex',
+	fontSize: '$small',
+	gap: '$xsmall',
+	variants: {
+		disabled: {
+			true: {
+				color: 'colorNeutralDisabledText',
+				cursor: 'not-allowed',
+				opacity: 0.5
+			}
+		}
+	}
 })
 
 export const inputStyle = css({
-    display: 'none',
+	display: 'none'
 })
 
 export const switchStyle = css({
-    aspectRatio: '2/1',
-    borderRadius: '$full',
-    borderStyle: 'solid',
-    borderWidth: '$thin',
-    display: 'flex',
-    height: '$xxsmall',
-    position: 'relative',
-    transition: 'background-color 0.3s ease, border 0.3s ease',
-    '&::after': {
-        aspectRatio: '1/1',
-        borderRadius: '$full',
-        boxShadow: '$medium',
-        content: '',
-        fontSize: '$xsmall',
-        height: '85%',
-        left: '5%',
-        position: 'absolute',
-        top: '50%',
-        transform: 'translateY(-50%)',
-        transition: 'left 0.3s ease, right 0.3s ease',
-    },
-    variants: {
-        checked: {
-            true: {
-                backgroundColor: '$colorAccent',
-                borderColor: '$colorAccent',
-                '&::after': {
-                    backgroundColor: '$colorNeutralPrimaryBackground',
-                    left: 'unset',
-                    right: '5%',
-                },
-            },
-            false: {
-                backgroundColor: 'transparent',
-                borderColor: '$colorNeutralBorder',
-                '&::after': {
-                    backgroundColor: '$colorNeutralPrimaryText',
-                },
-            },
-        },
-        disabled: {
-            true: {
-                borderColor: '$colorNeutralDisabledBackground',
-                '&::after': {
-                    backgroundColor: '$colorNeutralDisabledText',
-                },
-            },
-        },
-    },
-    compoundVariants: [
-        {
-            disabled: 'true',
-            checked: 'true',
-            css: {
-                backgroundColor: '$colorNeutralDisabledBackground',
-            },
-        },
-    ],
+	aspectRatio: '2/1',
+	borderRadius: '$full',
+	display: 'flex',
+	height: '$xxsmall',
+	position: 'relative',
+	transition: 'background-color 0.3s ease',
+	'&::after': {
+		aspectRatio: '1/1',
+		backgroundColor: '$colorNeutralPrimaryBackground',
+		borderRadius: '$full',
+		boxShadow: '$medium',
+		content: '',
+		fontSize: '$xsmall',
+		height: '85%',
+		left: '5%',
+		position: 'absolute',
+		top: '50%',
+		transform: 'translateY(-50%)'
+	},
+	variants: {
+		checked: {
+			true: {
+				backgroundColor: '$colorAccent',
+				'&::after': {
+					left: 'unset',
+					right: '5%'
+				}
+			},
+			false: {
+				backgroundColor: '$colorNeutralSecondaryBackground'
+			}
+		},
+		disabled: {
+			true: {
+				borderColor: '$colorNeutralDisabledBackground',
+				'&::after': {
+					backgroundColor: '$colorNeutralDisabledText'
+				}
+			}
+		}
+	},
+	compoundVariants: [
+		{
+			disabled: 'true',
+			checked: 'true',
+			css: {
+				backgroundColor: '$colorNeutralDisabledBackground'
+			}
+		}
+	]
 })
