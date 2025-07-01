9127054b691a800aa80018ff235b709aa654d9cb
felipe-coletti <felipecoletti2@gmail.com>
2025-07-01 20:28:07 -0300

    fix(tokens/typography): correct font weights

--- packages/styles/src/components/Heading.ts
+++ packages/styles/src/components/Heading.ts
@@ -1,25 +1,25 @@
 import { css } from '../stitches.config'
 
 export const headingStyle = css({
-    color: '$colorNeutralPrimaryText',
-    fontWeight: '$bold',
-    variants: {
-        size: {
-            xsmall: {
-                fontSize: '$xsmall'
-            },
-            small: {
-                fontSize: '$small'
-            },
-            medium: {
-                fontSize: '$medium'
-            },
-            large: {
-                fontSize: '$large'
-            },
-            xlarge: {
-                fontSize: '$xlarge'
-            }
-        }
-    }
+	color: '$colorNeutralPrimaryText',
+	fontWeight: '$extrabold',
+	variants: {
+		size: {
+			xsmall: {
+				fontSize: '$xsmall'
+			},
+			small: {
+				fontSize: '$small'
+			},
+			medium: {
+				fontSize: '$medium'
+			},
+			large: {
+				fontSize: '$large'
+			},
+			xlarge: {
+				fontSize: '$xlarge'
+			}
+		}
+	}
 })
