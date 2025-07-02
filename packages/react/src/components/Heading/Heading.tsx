40b351c7c627b989ed26688e12e37c5ebbdf1c9d
felipe-coletti <felipecoletti2@gmail.com>
2025-07-01 21:33:28 -0300

    fix(heading): make 'as' prop required in HeadingProps interface

--- packages/react/src/components/Heading/Heading.tsx
+++ packages/react/src/components/Heading/Heading.tsx
@@ -1,7 +1,7 @@
 import { HeadingProps } from './Heading.types'
 import { headingStyle } from '@strawberry-ui/styles/components/Heading'
 
-export const Heading = ({ size, as = 'h1', children }: HeadingProps) => {
+export const Heading = ({ as, size, children }: HeadingProps) => {
 	const Tag = as
 
 	return <Tag className={headingStyle({ size })}>{children}</Tag>
--- packages/react/src/components/Heading/Heading.types.tsx
+++ packages/react/src/components/Heading/Heading.types.tsx
@@ -1,5 +1,5 @@
 export interface HeadingProps {
+	as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 	size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge' | 'xxxlarge'
-	as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 	children: React.ReactNode
 }
