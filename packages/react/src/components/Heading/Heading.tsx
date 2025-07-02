7b9ae9efb18a035ce1694a6e67a71fdb2bd8d744
felipe-coletti <felipecoletti2@gmail.com>
2025-07-01 21:37:37 -0300

    refactor(heading): simplify 'as' prop usage in Heading component

--- packages/react/src/components/Heading/Heading.tsx
+++ packages/react/src/components/Heading/Heading.tsx
@@ -1,8 +1,6 @@
 import { HeadingProps } from './Heading.types'
 import { headingStyle } from '@strawberry-ui/styles/components/Heading'
 
-export const Heading = ({ as, size, children }: HeadingProps) => {
-	const Tag = as
-
+export const Heading = ({ as: Tag, size, children }: HeadingProps) => {
 	return <Tag className={headingStyle({ size })}>{children}</Tag>
 }
