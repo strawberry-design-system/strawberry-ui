3bc09ee8167bb2a3953319a0b53ca9dc3be1f455
felipe-coletti <felipecoletti2@gmail.com>
2025-07-01 21:41:34 -0300

    fix(heading): set default size to 'medium' in Heading component

--- packages/react/src/components/Heading/Heading.tsx
+++ packages/react/src/components/Heading/Heading.tsx
@@ -1,6 +1,6 @@
 import { HeadingProps } from './Heading.types'
 import { headingStyle } from '@strawberry-ui/styles/components/Heading'
 
-export const Heading = ({ as: Tag, size, children }: HeadingProps) => {
+export const Heading = ({ as: Tag, size = 'medium', children }: HeadingProps) => {
 	return <Tag className={headingStyle({ size })}>{children}</Tag>
 }
