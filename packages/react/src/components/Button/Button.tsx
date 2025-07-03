97c7c7810e2354141d2c6d39a4b6c581945d84d2
felipe-coletti <felipecoletti2@gmail.com>
2025-07-03 13:32:28 -0300

    refactor(button): remove unused 'textButton' variant and add 'onlyIcon' variant for button styles

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -3,53 +3,51 @@ import { ButtonProps, ButtonPropsWithType, LinkProps } from './Button.types'
 import { Spinner } from '../Spinner'
 
 export const Button = ({
-    as = 'button',
-    variant = 'primary',
-    size = 'medium',
-    fullWidth = false,
-    icon,
-    iconPosition = 'start',
-    children,
-    type = 'button',
-    disabled = false,
-    isLoading = false,
-    ...rest
+	as = 'button',
+	variant = 'primary',
+	size = 'medium',
+	fullWidth = false,
+	icon,
+	iconPosition = 'start',
+	children,
+	type = 'button',
+	disabled = false,
+	isLoading = false,
+	...rest
 }: ButtonProps) => {
-    const isTextButton = children != null
+	const buttonClasses = buttonStyle({
+		variant,
+		size,
+		fullWidth,
+		onlyIcon: !!icon && !children
+	})
 
-    const buttonClasses = buttonStyle({
-        variant,
-        size,
-        fullWidth,
-        textButton: isTextButton,
-    })
+	const content = isLoading ? (
+		<Spinner color='inherit' />
+	) : (
+		<>
+			{icon && iconPosition === 'start' && icon}
+			{children}
+			{icon && iconPosition === 'end' && icon}
+		</>
+	)
 
-    const content = isLoading ? (
-        <Spinner color='inherit' />
-    ) : (
-        <>
-            {icon && iconPosition === 'start' && icon}
-            {children}
-            {icon && iconPosition === 'end' && icon}
-        </>
-    )
+	if (as === 'a') {
+		return (
+			<a className={buttonClasses} {...(rest as LinkProps)}>
+				{content}
+			</a>
+		)
+	}
 
-    if (as === 'a') {
-        return (
-            <a className={buttonClasses} {...(rest as LinkProps)}>
-                {content}
-            </a>
-        )
-    }
-
-    return (
-        <button
-            type={type}
-            className={buttonClasses}
-            disabled={isLoading || disabled}
-            {...(rest as ButtonPropsWithType)}
-        >
-            {content}
-        </button>
-    )
+	return (
+		<button
+			type={type}
+			className={buttonClasses}
+			disabled={isLoading || disabled}
+			{...(rest as ButtonPropsWithType)}
+		>
+			{content}
+		</button>
+	)
 }
--- packages/styles/src/components/Button.ts
+++ packages/styles/src/components/Button.ts
@@ -1,121 +1,125 @@
 import { css } from '../stitches.config'
 
 export const buttonStyle = css({
-    alignItems: 'center',
-    borderRadius: '$full',
-    cursor: 'pointer',
-    display: 'inline-flex',
-    fontWeight: '$bold',
-    gap: '$xsmall',
-    justifyContent: 'center',
-    textDecoration: 'none',
-    transition: 'background-color 0.3s',
-    variants: {
-        variant: {
-            primary: {
-                backgroundColor: '$colorAccent',
-                border: 'none',
-                color: '$colorNeutralPrimaryBackground',
-                '&:not(:disabled):hover': {
-                    backgroundColor: '$colorAccentHover'
-                },
-                '&:disabled': {
-                    backgroundColor: '$colorNeutralDisabledBackground',
-                    color: '$colorNeutralDisabledText'
-                }
-            },
-            secondary: {
-                backgroundColor: 'transparent',
-                borderColor: '$colorNeutralBorder',
-                borderStyle: 'solid',
-                borderWidth: '$thin',
-                color: '$colorNeutralPrimaryText',
-                '&:not(:disabled):hover': {
-                    backgroundColor: '$colorNeutralHover'
-                },
-                '&:disabled': {
-                    borderColor: '$colorNeutralDisabledBackground',
-                    color: '$colorNeutralDisabledText'
-                }
-            },
-            tertiary: {
-                backgroundColor: 'transparent',
-                border: 'none',
-                color: '$colorNeutralPrimaryText',
-                '&:not(:disabled):hover': {
-                    backgroundColor: '$colorNeutralHover'
-                },
-                '&:disabled': {
-                    color: '$colorNeutralDisabledText'
-                }
-            },
-            negative: {
-                backgroundColor: 'transparent',
-                border: 'none',
-                color: '$colorNegative',
-                '&:not(:disabled):hover': {
-                    backgroundColor: '$colorNegative',
-                    border: 'none',
-                    color: '$colorNeutralPrimaryBackground'
-                },
-                '&:disabled': {
-                    color: '$colorNeutralDisabledText'
-                }
-            }
-        },
-        size: {
-            small: {
-                fontSize: '$xsmall',
-                padding: '$small'
-            },
-            medium: {
-                fontSize: '$small',
-                padding: '$medium'
-            },
-            large: {
-                fontSize: '$medium',
-                padding: '$large'
-            }
-        },
-        fullWidth: {
-            true: {
-                width: '$full'
-            }
-        },
-        textButton: {
-            true: {}
-        }
-    },
-    '&:disabled': {
-        cursor: 'not-allowed',
-        opacity: 0.5
-    },
-    compoundVariants: [
-        {
-            textButton: 'true',
-            size: 'small',
-            css: {
-                paddingInline: '$large'
-            }
-        },
-        {
-            textButton: 'true',
-            size: 'medium',
-            css: {
-                paddingInline: '$xlarge'
-            }
-        },
-        {
-            textButton: 'true',
-            size: 'large',
-            css: {
-                paddingInline: '$xxlarge'
-            }
-        }
-    ],
-    defaultVariants: {
-        variant: 'primary',
-        size: 'medium',
-        fullWidth: false
-    }
+	alignItems: 'center',
+	borderRadius: '$full',
+	cursor: 'pointer',
+	display: 'inline-flex',
+	fontWeight: '$bold',
+	gap: '$xsmall',
+	justifyContent: 'center',
+	textDecoration: 'none',
+	transition: 'background-color 0.3s',
+	variants: {
+		variant: {
+			primary: {
+				backgroundColor: '$colorAccent',
+				border: 'none',
+				color: '$colorNeutralPrimaryBackground',
+				'&:not(:disabled):hover': {
+					backgroundColor: '$colorAccentHover'
+				},
+				'&:disabled': {
+					backgroundColor: '$colorNeutralDisabledBackground',
+					color: '$colorNeutralDisabledText'
+				}
+			},
+			secondary: {
+				backgroundColor: 'transparent',
+				borderColor: '$colorNeutralBorder',
+				borderStyle: 'solid',
+				borderWidth: '$thin',
+				color: '$colorNeutralPrimaryText',
+				'&:not(:disabled):hover': {
+					backgroundColor: '$colorNeutralHover'
+				},
+				'&:disabled': {
+					borderColor: '$colorNeutralDisabledBackground',
+					color: '$colorNeutralDisabledText'
+				}
+			},
+			tertiary: {
+				backgroundColor: 'transparent',
+				border: 'none',
+				color: '$colorNeutralPrimaryText',
+				'&:not(:disabled):hover': {
+					backgroundColor: '$colorNeutralHover'
+				},
+				'&:disabled': {
+					color: '$colorNeutralDisabledText'
+				}
+			},
+			negative: {
+				backgroundColor: 'transparent',
+				border: 'none',
+				color: '$colorNegative',
+				'&:not(:disabled):hover': {
+					backgroundColor: '$colorNegative',
+					border: 'none',
+					color: '$colorNeutralPrimaryBackground'
+				},
+				'&:disabled': {
+					color: '$colorNeutralDisabledText'
+				}
+			}
+		},
+		size: {
+			small: {
+				fontSize: '$xsmall',
+				paddingBlock: '$small',
+				paddingInline: '$large'
+			},
+			medium: {
+				fontSize: '$small',
+				paddingBlock: '$medium',
+				paddingInline: '$xlarge'
+			},
+			large: {
+				fontSize: '$medium',
+				paddingBlock: '$large',
+				paddingInline: '$xxlarge'
+			}
+		},
+		fullWidth: {
+			true: {
+				width: '$full'
+			}
+		},
+		onlyIcon: {
+			true: {}
+		}
+	},
+	'&:disabled': {
+		cursor: 'not-allowed',
+		opacity: 0.5
+	},
+	compoundVariants: [
+		{
+			onlyIcon: 'true',
+			size: 'small',
+			css: {
+				padding: '$small'
+			}
+		},
+		{
+			onlyIcon: 'true',
+			size: 'medium',
+			css: {
+				padding: '$medium'
+			}
+		},
+		{
+			onlyIcon: 'true',
+			size: 'large',
+			css: {
+				padding: '$large'
+			}
+		}
+	],
+	defaultVariants: {
+		variant: 'primary',
+		size: 'medium',
+		fullWidth: false,
+		onlyIcon: false
+	}
 })
